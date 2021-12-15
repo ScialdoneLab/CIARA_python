3aedc4d76790153eb2f15ae9498452cb2cc72dfe
Marco Stock <stk.mrc@gmail.com>
2021-12-15 16:43:37 +0100

    Speed up + Markdown comments

--- src/EntropyOfMixing/functions_entropy_mixing.py
+++ src/EntropyOfMixing/functions_entropy_mixing.py
@@ -26,9 +26,8 @@ def perform_sub_entropy(nn_gene_expression, binary_expression, p_value, odds_rat
 
     return entropy_nn, p_value_nn
 
-def entropy_mixing_gene(gene, norm_adata, knn_matrix, p_value, odds_ratio, local_region, approximation):
+def entropy_mixing_gene(gene_expression, knn_matrix, p_value, odds_ratio, local_region, approximation):
 
-    gene_expression = norm_adata.X[:, norm_adata.var_names == gene].flatten()
     binary_expression = gene_expression > np.median(gene_expression)
     
     if approximation:
@@ -58,21 +57,22 @@ def entropy_mixing(norm_adata, knn_matrix, n_cores, p_value, odds_ratio, local_r
     assert((norm_adata.obs_names == knn_matrix.index.values).all)
     #knn_matrix is pandas dataframe in same orientation as Anndata.X (cellsxgenes)
     #knn_matrix and Anndata.X should contain the cells in same order
-
-    background = norm_adata.var_names[norm_adata.var["EOM_background"]]
+    
+    background = norm_adata.X[:, norm_adata.var["EOM_background"]]
+    gene_expressions = [background[:,i].flatten() for i in range(np.shape(background)[1])]
     knn_matrix = knn_matrix.to_numpy()
     
     pool = multiprocessing.Pool(n_cores)
-    temp = partial(entropy_mixing_gene, norm_adata=norm_adata, knn_matrix=knn_matrix, p_value=p_value, odds_ratio=odds_ratio, local_region=local_region, approximation=approximation)
-    results = pool.map(func=temp, iterable=background, chunksize=50)
+    temp = partial(entropy_mixing_gene, knn_matrix=knn_matrix, p_value=p_value, odds_ratio=odds_ratio, local_region=local_region, approximation=approximation)
+    results = pool.map(func=temp, iterable=gene_expressions, chunksize=50)
     pool.close()
     pool.join()
     
     entropies_output = [np.NAN for i in range(len(norm_adata.var_names))]
     p_values_output = [np.NAN for i in range(len(norm_adata.var_names))]
-    for index, gene in enumerate(background):
-        entropies_output[norm_adata.var_names.get_loc(gene)] = results[index][0]
-        p_values_output[norm_adata.var_names.get_loc(gene)] = results[index][1]
+    for index, gene_pos in enumerate(np.where(norm_adata.var["EOM_background"])[0]):
+        entropies_output[gene_pos] = results[index][0]
+        p_values_output[gene_pos] = results[index][1]
     
     print('\n---- Finished sucessfully! ----')
 
